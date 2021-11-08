b33814b8b62fe0c2d47b1dc5ccf3b3af37170a77
Wirstblase <mariussuflea13@icloud.com>
2021-11-08 12:44:59 +0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.javafxtest {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafxtest to javafx.fxml;
+    exports com.example.javafxtest;
+}
